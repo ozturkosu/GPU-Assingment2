62dd8a7230b99556c088438bcda7cd7c12b37073
ozturkosu <ozturk.osu@gmail.com>
2018-11-25 08:16:55 -0500

    added check

--- assign2_template/spmm_csr_driver.cu
+++ assign2_template/spmm_csr_driver.cu
@@ -265,7 +265,7 @@ int main(int argc, char *argv[]) {
     //print_dmat(dmat_out, mat.nrows , K);
     //std::cout << "GPU\n";
     //print_dmat(dmat_out_GPU,  mat.nrows , K);
-    //check_dmat(dmat_out, dmat_out_GPU, mat.nrows, K);
+    check_dmat(dmat_out, dmat_out_GPU, mat.nrows, K);
 
     //print_dmat(dmat_out, mat.nrows, K);
 
