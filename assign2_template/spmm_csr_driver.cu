8a8c78450765b8d6b634137528848371cb0c9deb
ozturkosu <ozturk.osu@gmail.com>
2018-11-26 22:30:47 -0500

    a

--- assign2_template/spmm_csr_driver.cu
+++ assign2_template/spmm_csr_driver.cu
@@ -249,7 +249,8 @@ int main(int argc, char *argv[]) {
 
     //Initialize the Grid and Block Dimension
 
-    dim3 dimGrid( (K-1) / TILE_WIDTH + 1 , ( mat.nrows-1)/TILE_WIDTH +1 , 1  ) ;
+    //dim3 dimGrid( (K-1) / TILE_WIDTH + 1 , ( mat.nrows-1)/TILE_WIDTH +1 , 1  ) ;
+    dim3 dimGrid( ( mat.nrows-1)/TILE_WIDTH +1 , (K-1) / TILE_WIDTH + 1 ,  1  ) ;
     dim3 dimBlock(TILE_WIDTH , TILE_WIDTH , 1) ;
 
     cudaEventRecord(startEvent, 0);
