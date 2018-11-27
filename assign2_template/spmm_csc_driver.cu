95edce759f93514db4bc633e88382112eb243bac
ozturkosu <ozturk.osu@gmail.com>
2018-11-27 03:01:06 -0500

    a

--- assign2_template/spmm_csc_driver.cu
+++ assign2_template/spmm_csc_driver.cu
@@ -219,6 +219,7 @@ int main(int argc, char *argv[]) {
 
     //dim3 dimGrid((K-1) / TILE_WIDTH + 1 , (mat.ncols -1)/TILE_WIDTH +1 , 1  ) ;
     //
+    //dim3 dimGrid( (K-1) / TILE_WIDTH +1  , (mat.ncols -1)/TILE_WIDTH+1 , 1  ) ;
     dim3 dimGrid( (K-1) / TILE_WIDTH +1  , (mat.ncols -1)/TILE_WIDTH+1 , 1  ) ;
     dim3 dimBlock(TILE_WIDTH , TILE_WIDTH , 1) ;
 
