5ff9f53d4f5c0a76025d55500b67df9157de28bb
ozturkosu <ozturk.osu@gmail.com>
2018-11-26 21:18:11 -0500

    a

--- assign2_template/spmm_csc_driver.cu
+++ assign2_template/spmm_csc_driver.cu
@@ -250,11 +250,11 @@ int main(int argc, char *argv[]) {
 
     //std::cout << "replace one argument to the below function with the values from gpu " << std::endl;
 
-    //std::cout << "replace one argument to the below function with the values from gpu " << std::endl;
-    //std::cout << "CPU\n";
-    //print_dmat(dmat_out, mat.nrows , K);
-    //std::cout << "GPU\n";
-    //print_dmat(dmat_out_GPU,  mat.nrows , K);
+    std::cout << "replace one argument to the below function with the values from gpu " << std::endl;
+    std::cout << "CPU\n";
+    print_dmat(dmat_out, mat.nrows , K);
+    std::cout << "GPU\n";
+    print_dmat(dmat_out_GPU,  mat.nrows , K);
 
     check_dmat(dmat_out, dmat_out_D, mat.nrows, K);
 
