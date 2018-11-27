f5b83ab872a7e490f15798233198860701c04481
ozturkosu <ozturk.osu@gmail.com>
2018-11-27 02:01:53 -0500

    a

--- assign2_template/spmm_opt_driver.cu
+++ assign2_template/spmm_opt_driver.cu
@@ -177,15 +177,16 @@ __global__ void dev_opt_spmm(unsigned int * deviceCSRrow_indx , unsigned int * d
       //const int irow= warp_id / K ;
       //const int icol= warp_id & (K-1) ;
 
-      //const int warp_idx = blockIdx.x;
-      //const int warp_idy = blockIdx.y*blockDim.y + threadIdx.y;
+      const int warp_idx = blockIdx.x;
+      const int warp_idy = blockIdx.y*blockDim.y + threadIdx.y;
 
-      //const int irow = warp_idy *(128/K) + warp_idx/K;
-      //const int icol = warp_idx % K;
+      const int irow = warp_idy /2;
+      const int icol = warp_idx % K;
 
-      const int warpId= 128 * blockIdx.y + blockIdx.x * 32 + threadIdx.y;
-      const int irow = warpId /K ;
-      const int icol= warpId % K;
+      //const int warpId= 128 * blockIdx.y + blockIdx.x * 32 + threadIdx.y;
+
+      //const int irow = warpId /K ;
+      //const int icol= warpId % K;
 
       int lane = thread_id_x & (31) ;
 
