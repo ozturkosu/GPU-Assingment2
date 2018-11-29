3b05c614df472463afc099ae9ff3f6546e40f73a
ozturkosu <ozturk.osu@gmail.com>
2018-11-29 04:53:38 -0500

    a

--- assign2_template/spmm_csr_driver.cu
+++ assign2_template/spmm_csr_driver.cu
@@ -269,7 +269,7 @@ int main(int argc, char *argv[]) {
         const int start = i * CHUNK_SIZE ;
         //const int end  = min(mat.nrows , (i +1) * CHUNK_SIZE) ;
 
-        const int end;
+        int end;
 
         if(mat.nrows < (i +1) * CHUNK_SIZE))
             end = mat.nrows;
